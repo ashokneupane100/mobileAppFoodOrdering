7357378a0d80882211d64203c4ee4b64dae3d1ec
Ashok Neupane <ashokabrother@gmail.com>
2025-04-22 06:55:02 +0545

    Create Custom Component

---
+++ src/app/(tabs)/index.jsx
@@ -0,0 +1,21 @@
+// Assuming this file is at src/screens/TabOneScreen.jsx
+import {View} from 'react-native';
+
+import products from '../../../assets/data/products'
+
+import ProductListItem from '@/src/components/ProductListItem';
+
+
+export default function MenuScreen(){
+  return(
+    <View>
+      <ProductListItem product={products[5]} />
+      <ProductListItem product={products[0]} />
+
+    </View>
+  )
+}
+
+
+
+
