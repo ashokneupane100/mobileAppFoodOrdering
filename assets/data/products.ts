250ef6551e74290bad902b4c9d826087f9755eda
Ashok Neupane <ashokneupane100>
2025-04-23 12:12:54 +0545

    changed to Nepali Price

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -2,72 +2,62 @@ const products = [
   {
     id: 1,
     name: 'Ultimate Pepperoni',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 12.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 1734.32, // 12.99 * 133.50
   },
   {
     id: 2,
     name: 'ExtravaganZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 14.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 1999.17, // 14.99 * 133.50
   },
   {
     id: 3,
     name: 'MeatZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 13.47,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 1798.25, // 13.47 * 133.50
   },
   {
     id: 4,
     name: 'Margarita',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: 9.9,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+    price: 1321.65, // 9.9 * 133.50
   },
   {
     id: 5,
     name: 'Pacific Veggie',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 1734.32, // 12.99 * 133.50
   },
   {
     id: 6,
     name: 'Hawaiian',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: 10.49,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+    price: 1400.42, // 10.49 * 133.50
   },
   {
     id: 7,
     name: 'Deluxe',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: 16.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+    price: 2268.17, // 16.99 * 133.50
   },
   {
     id: 8,
     name: 'BBQ Chicken',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.89,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 1720.82, // 12.89 * 133.50
   },
   {
     id: 9,
     name: 'Chicken Bacon Ranch',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 13.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 1867.67, // 13.99 * 133.50
   },
   {
     id: 10,
     name: '6 Cheese',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: 13.29,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+    price: 1774.22, // 13.29 * 133.50
   },
 ];
 
