265aded7895a666b5b216ecc9400dad3a6f45231
Ashok Neupane <ashokabrother@gmail.com>
2025-04-28 05:08:21 +0545

    changed the header

--- src/app/(tabs)/menu/_layout.jsx
+++ src/app/(tabs)/menu/_layout.jsx
@@ -1,5 +1,31 @@
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 
-export default function MenuStack(){
-    return <Stack><Stack.Screen name="index" options={{title:"Menu"}} /></Stack>
+export default function MenuStack() {
+  const headerStyle = {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#00008B",
+    textAlign: "center",
+  };
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => (
+            <View style={{ alignItems: 'center' }}>
+              {["Pizza App By:", "💓Ashok Neupane💓", "-Made with Love"].map((text, index) => (
+                <Text key={index} style={headerStyle}>
+                  {text}
+                </Text>
+              ))}
+            </View>
+          ),
+          headerTitleAlign: "center",
+        }}
+      />
+    </Stack>
+  );
 }
